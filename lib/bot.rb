1ddb34c5d1aa78c1fd109eaace756b63082a0fae
Daniel Ronan <danielconnorronan@gmail.com>
2020-09-24 11:25:05 +0100

    added 7 commands

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,6 +4,12 @@ module PremierLeague
       title 'Premier League App'
       desc 'This app brings you the relative information on the Premier League'
 
+      command :fantasy do
+        title 'fantasy'
+        desc 'Returns the \'Fantasy Football\' page'
+        long_desc 'Returns the \'Fantasy Football\' page'
+      end
+
       command :fixtures do
         title 'fixtures'
         desc 'Returns the \'Fixtures\' page'
@@ -20,6 +26,42 @@ module PremierLeague
         title 'results'
         desc 'Returns the \'Results\' page'
         long_desc 'Returns the \'Results\' page'
+      end
+
+      command :tables do
+        title 'tables'
+        desc 'Returns the \'Tables\' page'
+        long_desc 'Returns the \'Tables\' page'
+      end
+
+      command :transfers do
+        title 'transfers'
+        desc 'Returns the \'Transfers\' news page'
+        long_desc 'Returns the \'Transfers\' news page'
+      end
+
+      command :tickets do
+        title 'tickets'
+        desc 'Returns the \'Tickets\' page'
+        long_desc 'Returns the \'Tickets\' page'
+      end
+
+      command :players do
+        title 'players'
+        desc 'Returns the \'Players\' page'
+        long_desc 'Returns the \'Players\' page'
+      end
+
+      command :managers do
+        title 'managers'
+        desc 'Returns the \'Managers\' page'
+        long_desc 'Returns the \'Managers\' page'
+      end
+
+      command :news do
+        title 'news'
+        desc 'Returns the \'News\' page'
+        long_desc 'Returns the \'News\' page'
       end
     end
   end  
--- lib/commands/get_prem.rb
+++ lib/commands/get_prem.rb
@@ -2,6 +2,10 @@ require 'open-uri'
 
 module PremierLeague
   class GetPrem < SlackRubyBot::Commands::Base
+    command 'fantasy' do |client, data, _match|
+      client.say(channel: data.channel, text: 'https://fantasy.premierleague.com/')
+    end
+
     command 'fixtures' do |client, data, _match|
       client.say(channel: data.channel, text: 'https://www.premierleague.com/fixtures')
     end
@@ -12,6 +16,30 @@ module PremierLeague
 
     command 'results' do |client, data, _match|
       client.say(channel: data.channel, text: 'https://www.premierleague.com/results')
+    end
+
+    command 'tables' do |client, data, _match|
+      client.say(channel: data.channel, text: 'https://www.premierleague.com/tables')
+    end
+
+    command 'transfers' do |client, data, _match|
+      client.say(channel: data.channel, text: 'https://www.premierleague.com/transfers')
+    end
+
+    command 'tickets' do |client, data, _match|
+      client.say(channel: data.channel, text: 'https://www.premierleague.com/tickets')
+    end
+
+    command 'players' do |client, data, _match|
+      client.say(channel: data.channel, text: 'https://www.premierleague.com/players')
+    end
+
+    command 'managers' do |client, data, _match|
+      client.say(channel: data.channel, text: 'https://www.premierleague.com/managers')
+    end
+
+    command 'news' do |client, data, _match|
+      client.say(channel: data.channel, text: 'https://www.premierleague.com/news')
     end
   end
 end
