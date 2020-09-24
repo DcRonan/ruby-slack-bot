7d027247d3cfd068c4aa2cb091be8da4bfddb643
Daniel Ronan <danielconnorronan@gmail.com>
2020-09-24 15:15:05 +0100

    added 9 test cases to get_prem_spec.rb

--- spec/get_prem_spec.rb
+++ spec/get_prem_spec.rb
@@ -37,7 +37,7 @@ RSpec::Matchers.define :respond_with_slack_message do |expected|
 end
 
 describe SlackRubyBot::Commands do
-  it 'Checks if the Bot returns the link for \'fantasy\'' do
+  it 'Checks if the Bot returns a message for \'fantasy\'' do
     expect(message: "#{SlackRubyBot.config.user} fantasy").to respond_with_slack_message
   end
 
@@ -45,40 +45,76 @@ describe SlackRubyBot::Commands do
     expect(message: "#{SlackRubyBot.config.user} fantasy").to respond_with_slack_message('https://fantasy.premierleague.com/')
   end
 
-  it 'Checks if the Bot returns the link for \'fixtures\'' do
+  it 'Checks if the Bot returns a message for \'fixtures\'' do
     expect(message: "#{SlackRubyBot.config.user} fixtures").to respond_with_slack_message
   end
 
-  it 'Checks if the Bot returns the link for \'clubs\'' do
-    expect(message: "#{SlackRubyBot.config.user} fantasy").to respond_with_slack_message
+  it 'Checks if the Bot returns a message for \'fixtures\'' do
+    expect(message: "#{SlackRubyBot.config.user} fixtures").to respond_with_slack_message('https://www.premierleague.com/fixtures')
   end
 
-  it 'Checks if the Bot returns the link for \'results\'' do
+  it 'Checks if the Bot returns a message for \'clubs\'' do
+    expect(message: "#{SlackRubyBot.config.user} clubs").to respond_with_slack_message
+  end
+
+  it 'Checks if the Bot returns the link for \'clubs\'' do
+    expect(message: "#{SlackRubyBot.config.user} clubs").to respond_with_slack_message('https://www.premierleague.com/clubs')
+  end
+
+  it 'Checks if the Bot returns a message for \'results\'' do
     expect(message: "#{SlackRubyBot.config.user} results").to respond_with_slack_message
   end
 
-  it 'Checks if the Bot returns the link for \'tables\'' do
+  it 'Checks if the Bot returns the link for \'results\'' do
+    expect(message: "#{SlackRubyBot.config.user} results").to respond_with_slack_message('https://www.premierleague.com/results')
+  end
+
+  it 'Checks if the Bot returns a message for \'tables\'' do
     expect(message: "#{SlackRubyBot.config.user} tables").to respond_with_slack_message
   end
 
-  it 'Checks if the Bot returns the link for \'transfers\'' do
+  it 'Checks if the Bot returns the link for \'tables\'' do
+    expect(message: "#{SlackRubyBot.config.user} tables").to respond_with_slack_message('https://www.premierleague.com/tables')
+  end
+
+  it 'Checks if the Bot returns ta message for \'transfers\'' do
     expect(message: "#{SlackRubyBot.config.user} transfers").to respond_with_slack_message
   end
 
-  it 'Checks if the Bot returns the link for \'tickets\'' do
+  it 'Checks if the Bot returns the link for \'transfers\'' do
+    expect(message: "#{SlackRubyBot.config.user} transfers").to respond_with_slack_message('https://www.premierleague.com/transfers')
+  end
+
+  it 'Checks if the Bot returns a message for \'tickets\'' do
     expect(message: "#{SlackRubyBot.config.user} tickets").to respond_with_slack_message
   end
 
-  it 'Checks if the Bot returns the link for \'players\'' do
+  it 'Checks if the Bot returns the link for \'tickets\'' do
+    expect(message: "#{SlackRubyBot.config.user} tickets").to respond_with_slack_message('https://www.premierleague.com/tickets')
+  end
+
+  it 'Checks if the Bot returns a message for \'players\'' do
     expect(message: "#{SlackRubyBot.config.user} players").to respond_with_slack_message
   end
 
-  it 'Checks if the Bot returns the link for \'managers\'' do
+  it 'Checks if the Bot returns the link for \'players\'' do
+    expect(message: "#{SlackRubyBot.config.user} players").to respond_with_slack_message('https://www.premierleague.com/players')
+  end
+
+  it 'Checks if the Bot returns a message for \'managers\'' do
     expect(message: "#{SlackRubyBot.config.user} managers").to respond_with_slack_message
+  end
+
+  it 'Checks if the Bot returns the link for \'managers\'' do
+    expect(message: "#{SlackRubyBot.config.user} managers").to respond_with_slack_message('https://www.premierleague.com/managers')
   end
 
   it 'Checks if the Bot returns the link for \'news\'' do
     expect(message: "#{SlackRubyBot.config.user} news").to respond_with_slack_message
+  end
+
+  it 'Checks if the Bot returns the link for \'news\'' do
+    expect(message: "#{SlackRubyBot.config.user} news").to respond_with_slack_message('https://www.premierleague.com/news')
   end
 end
 
